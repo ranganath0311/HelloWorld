86bec6211e4360aac4f89e31b779b84fba44235a
Ranganath S <46857280+ranganath0311@users.noreply.github.com>
2022-01-01 22:33:01 +0530

    Create Web Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+const port = 3000;
+app.listen(port);
+console.log("Listening on port "+port );
